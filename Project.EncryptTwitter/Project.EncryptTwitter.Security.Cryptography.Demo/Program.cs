d211e97f666a182d6d793135fa0e7de0df827133
dance2die <dontbothermewithspam@gmail.com>
2015-08-03 11:34:11 -0400

    Using source from Pluralsight: http://www.pluralsight.com/courses/exercise-files/practical-cryptography-dotnet

---
+++ Project.EncryptTwitter/Project.EncryptTwitter.Security.Cryptography.Demo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using CryptographyInDotNet;
+
+namespace Project.EncryptTwitter.Security.Cryptography.Demo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string original = "Very secret and important information that can not fall into the wrong hands.";
+
+			var hybrid = new HybridEncryption();
+
+			var rsaParams = new RSAWithRSAParameterKey();
+			rsaParams.AssignNewKey();
+
+			var digitalSignature = new DigitalSignature();
+			digitalSignature.AssignNewKey();
+
+			Console.WriteLine("Hybrid Encryption with Integrity Check Demonstration in .NET");
+			Console.WriteLine("------------------------------------------------------------");
+			Console.WriteLine();
+
+			try
+			{
+				var encryptedBlock = hybrid.EncryptData(
+					Encoding.UTF8.GetBytes(original), rsaParams, digitalSignature);
+
+				var decrpyted = hybrid.DecryptData(encryptedBlock, rsaParams, digitalSignature);
+
+				Console.WriteLine("Original Message = " + original);
+				Console.WriteLine();
+				Console.WriteLine("Message After Decryption = " + Encoding.UTF8.GetString(decrpyted));
+			}
+			catch (CryptographicException ex)
+			{
+				Console.WriteLine("Error : " + ex.Message);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
