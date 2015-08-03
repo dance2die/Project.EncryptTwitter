e8dbfca45e2850c32a2674f3b21d0b35cff756d9
dance2die <dontbothermewithspam@gmail.com>
2015-08-03 12:50:37 -0400

    compressing encrypted text makes the size bigger!!!

--- Project.EncryptTwitter/Project.EncryptTwitter.Security.Cryptography.Demo/Program.cs
+++ Project.EncryptTwitter/Project.EncryptTwitter.Security.Cryptography.Demo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -13,7 +15,9 @@ namespace Project.EncryptTwitter.Security.Cryptography.Demo
 		static void Main(string[] args)
 		{
 			//const string original = "Very secret and important information that can not fall into the wrong hands.";
-			string original = new String('0', 140);
+			//string original = new String('0', 140);
+			//string original = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
+			string original = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
 
 			var hybrid = new HybridEncryption();
 
@@ -34,10 +38,19 @@ namespace Project.EncryptTwitter.Security.Cryptography.Demo
 
 				var decrpyted = hybrid.DecryptData(encryptedBlock, rsaParams, digitalSignature);
 
+				//byte[] gzippedBytes = GetGZippedBytes(encryptedBlock.EncryptedData);
+				//byte[] ungzippedBytes = GetUnGZippedBytes(gzippedBytes);
+				byte[] gzippedBytes = Compress(encryptedBlock.EncryptedData);
+				byte[] ungzippedBytes = Decompress(gzippedBytes);
+
 				Console.WriteLine("Original Message = " + original);
 				Console.WriteLine("Original Message Length: {0}", original.Length);
 				Console.WriteLine("Encrypted Data: {0}", Convert.ToBase64String(encryptedBlock.EncryptedData));
 				Console.WriteLine("Encrypted Data Size: {0}", encryptedBlock.EncryptedData.Length);
+				Console.WriteLine("GZipped Encrypted Data: {0}", Convert.ToBase64String(gzippedBytes));
+				Console.WriteLine("GZipped Encrypted Data Size: {0}", gzippedBytes.Length);
+				Console.WriteLine("UnGZipped Encrypted Data: {0}", Convert.ToBase64String(ungzippedBytes));
+				Console.WriteLine("UnGZipped Encrypted Data Size: {0}", ungzippedBytes.Length);
 				Console.WriteLine();
 				Console.WriteLine("Message After Decryption = " + Encoding.UTF8.GetString(decrpyted));
 			}
@@ -47,6 +60,71 @@ namespace Project.EncryptTwitter.Security.Cryptography.Demo
 			}
 
 			Console.ReadLine();
+		}
+
+		private static byte[] GetGZippedBytes(byte[] data)
+		{
+			using (MemoryStream outStream = new MemoryStream())
+			using (GZipStream gZipStream = new GZipStream(outStream, CompressionMode.Compress, false))
+			using (MemoryStream inStream = new MemoryStream(data))
+			{
+				//gz.Write(data, 0, data.Length);
+				inStream.CopyTo(gZipStream);
+				return outStream.ToArray();
+			}
+		}
+		private static byte[] GetUnGZippedBytes(byte[] data)
+		{
+			using (MemoryStream inputStream = new MemoryStream(data))
+			using (GZipStream gZipStream = new GZipStream(inputStream, CompressionMode.Decompress, false))
+			using (MemoryStream outputStream = new MemoryStream())
+			{
+				//gZipStream.Read(data, 0, data.Length);
+				//return inputStream.ToArray();
+				gZipStream.CopyTo(outputStream);
+				return outputStream.ToArray();
+			}
+		}
+
+		public static byte[] Compress(byte[] buffer)
+		{
+			MemoryStream ms = new MemoryStream();
+			using (GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true))
+			{
+				zip.Write(buffer, 0, buffer.Length);
+			}
+
+			ms.Position = 0;
+			MemoryStream outStream = new MemoryStream();
+
+			byte[] compressed = new byte[ms.Length];
+			ms.Read(compressed, 0, compressed.Length);
+
+			byte[] gzBuffer = new byte[compressed.Length + 4];
+			System.Buffer.BlockCopy(compressed, 0, gzBuffer, 4, compressed.Length);
+			System.Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzBuffer, 0, 4);
+			//return Convert.ToBase64String(gzBuffer);
+			return gzBuffer;
+		}
+
+		public static byte[] Decompress(byte[] gzBuffer)
+		{
+			using (MemoryStream ms = new MemoryStream())
+			{
+				int msgLength = BitConverter.ToInt32(gzBuffer, 0);
+				ms.Write(gzBuffer, 4, gzBuffer.Length - 4);
+
+				byte[] buffer = new byte[msgLength];
+
+				ms.Position = 0;
+				using (GZipStream zip = new GZipStream(ms, CompressionMode.Decompress))
+				{
+					zip.Read(buffer, 0, buffer.Length);
+				}
+
+				//return Encoding.UTF8.GetString(buffer);
+				return buffer;
+			}
 		}
 	}
 }
